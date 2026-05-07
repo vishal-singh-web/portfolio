8c0fc880b66b1231f89be02478e4e07385f968d6
Vishal Singh <vishalu.cifer@gmail.com>
2026-05-07 14:11:34 +0530

    minor changes

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,5 +1,2 @@
-import projects from '../data/projects.json'
-import certificates from '../data/certificates.json'
-
-export const TOP_PROJECTS = projects as any
-export const CERTIFICATES = certificates as any
+export const TOP_PROJECTS = [] as any
+export const CERTIFICATES = [] as any
